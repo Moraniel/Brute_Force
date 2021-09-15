bfe793d25c6d5949dbf7262fa9eb7400e8dbc71b
Moraniel <79228030+Moraniel@users.noreply.github.com>
2021-09-15 11:47:15 -0300

    Update Read_File_Coordenadas.py

--- Read_File_Coordenadas.py
+++ Read_File_Coordenadas.py
@@ -8,7 +8,7 @@ def readFileCoord(filename):
     for i in range(len(texto)):  # Esse for percorre a posições dp vetor texto
       
         matriz.append(texto[i].split())
-    for j in range(len(matriz)):      # Aqui a lista de coordenadas são transformadas em float
+    for j in range(len(matriz)):      # Aqui a lista de coordenadas são transformadas em float para serem lidas no algoritimo de Christofides.
       for k in range(len(matriz[j])):
         matriz[j][k]=(float(matriz[j][k]))
 
